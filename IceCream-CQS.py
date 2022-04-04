8d94399ad4bb629601d80a5865c1e9c95af0b943
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-04-04 09:46:41 +0100

    refactored Python IceCream-CQS

--- IceCream-CQS.py
+++ IceCream-CQS.py
@@ -1,11 +1,28 @@
-class IceCream():
-    def set_temperature(temperature):
-        pass
+global temperature_readings
+global current_temperature
 
-    def getTemperature(self):
-        pass
 
-    def alert(temperature):
-        pass
+def set_temperature(temperature):
+    if temperature > 10:
+        temperature = 10
 
+    temperature_readings.append(temperature)
+    current_temperature = temperature
+
+
+def get_current_temperature(self):
+    return current_temperature
+
+
+def start_monitor():
+    if current_temperature > 10:
+        alert(current_temperature)
+
+
+def get_temperature_readings():
+    return temperature_readings
+
+
+def alert(temperature):
+    print(f'Ice ream mahcine too warm. Temperature: {temperature}')
 
